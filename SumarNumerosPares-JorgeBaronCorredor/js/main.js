b0e163ff4390eaf7729590f5ff00f7274d2b6fc1
SENA <SENA@NEIDFPSITFSP828.FormacionSena.RED>
2024-04-25 16:43:31 -0500

    JORGE

--- SumarNumerosPares-JorgeBaronCorredor/js/main.js
+++ SumarNumerosPares-JorgeBaronCorredor/js/main.js
@@ -10,3 +10,20 @@ function sumNumPar(pnum) {
         return suma;
     }
 }
+
+function sumNumImp(pnum) {
+    let num = pnum;
+    let suma = 0;
+    if (num < 1 || isNaN(num)) {
+    return "El número ingresado no es válido. Debe ser un número entero positivo.";
+    } else {
+        for (let i = 1; i <= num; i = i + 1) {
+            if (i % 2 == 0) {
+                suma = suma
+            }else{
+                suma = suma + i;
+            }
+        }
+        return suma;
+    }
+}
