a48bebbcdd1fd9bd4a3d7a28219f2de684e5217a
SENA <SENA@NEIDFDSITFSD712.FormacionSena.RED>
2024-04-25 15:01:30 -0500

    terminado

--- tablaMulti-JesusFernandoCarvajal/js/main.js
+++ tablaMulti-JesusFernandoCarvajal/js/main.js
@@ -7,6 +7,15 @@ window.addEventListener('load', ()=>{
         console.log("                TABLAS MULTIPLICAR        ")
         console.log("==========================================")
     }
+  
+
+    document.getElementById("mostrar").addEventListener('click', ()=>{
+        document.getElementById("formulario").style.display="block";
+    })
+    
+    document.getElementById("limpiar").addEventListener('click', ()=>{
+        document.getElementById("resultado").innerHTML = "";
+    })
     
     document.getElementById("calcular").addEventListener('click', ()=>{
         let tabla = document.getElementById("tablaNumber").value;
