71c2c7be25935108ccc37091370490dc9a4a98c8
ingrid0208 <161946128+ingrid0208@users.noreply.github.com>
2024-04-25 15:12:51 -0500

    Add files via upload

---
+++ almuerzo-ingridMedina/js/comida.js
@@ -0,0 +1,90 @@
+let comidaop
+let tipo_comidaop
+let precio
+let cantidad
+let total
+let comida
+// console.log (" ingreser nombre del cliente")
+nombre = prompt("ingreser nombre del cliente");
+
+comida = prompt(" 1-almuerzos completo 2-bandeja  3-sopa ");
+
+
+if (comida == 1) {
+	comidaop = prompt("1- sancocho y pollo sudado 2- crema de verdura y alitas BBQQ 3- sopa de maiz y pechuga gratinada")
+
+} else if (comida == 2) {
+	comidaop = prompt("1- pollo sudado 2- alitas BBQQ 3- pechuga gratinada")
+
+} else if (comida == 3) {
+	comidaop = prompt("1- sancocho 2- crema de verdura 3- sopa de maiz")
+} else{
+	alert("Seleccion incorrecta ")
+	}
+
+if (comidaop = 1) {
+
+} if (comidaop == 1) {
+
+	precio = 12000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+
+alert("El precio es: " + total)
+	
+} else if (comidaop == 2) {
+	precio = 17000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else if (comidaop == 3) {
+	precio = 19000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else{
+	alert("seleccion incorecta")
+}
+
+if (comidaop = 2) {
+} if (comidaop == 1) {
+	precio = 8000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else if (comidaop == 2) {
+	precio = 12000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else if (comidaop == 3) {
+	precio = 15000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else {
+
+	alert("seleccion incorecta")
+}
+if (comidaop = 3) {
+} if (comidaop == 1) {
+	precio = 4000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else if (comidaop == 2) {
+	precio = 5000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else if (comidaop == 3) {
+	precio = 4000
+	alert(precio)
+	cantidad = prompt("ingre la cantidad ")
+	total = precio * cantidad
+} else {
+	alert("seleccion incorecta")
+}
+
+
